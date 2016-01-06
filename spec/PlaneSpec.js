2f7f6ff4f4a54c01fb8b125603f7b8cd2da86c76
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-06 10:37:28 +0000

    Adds contexts "stormy" & "sunny"

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -12,43 +12,55 @@ describe("Plane", function() {
     };
   });
 
-  it("A plane is initialized as flying", function() {
-    expect(plane.isFlying).toEqual(true);
-  });
-
-  describe('#land', function() {
-    it("confirms landing with a message", function() {
-      landingMessage = 'The plane has landed at the airport';
-      expect(plane.land(airport)).toEqual(landingMessage);
-    });
-
-    it('confirms the plane is not flying', function() {
-      plane.land(airport);
-      expect(plane.isFlying).toEqual(false);
-    });
-  });
-
-  describe('#takeOff', function() {
-
-    beforeEach(function() {
-      plane.land(airport);
-    });
-
-    it('raises an error when the weather is stormy', function() {
-      error = "Cannot take off when weather is stormy";
-      plane1 = new Plane();
-      spyOn(plane1.weather, 'isStormy').and.returnValue(true);
-      expect(function () { plane1.takeOff(airport); }).toThrowError( error );
-    });
-
-    it('confirms the plane is flying', function() {
-      plane.takeOff(airport);
+  describe('Weather is sunny', function() {
+    it("A plane is initialized as flying", function() {
       expect(plane.isFlying).toEqual(true);
     });
 
-    it('confirms departure with a message', function() {
-      departureMessage = 'The plane has departed the airport';
-      expect(plane.takeOff(airport)).toEqual(departureMessage);
+    describe('#land', function() {
+      it("confirms landing with a message", function() {
+        landingMessage = 'The plane has landed at the airport';
+        expect(plane.land(airport)).toEqual(landingMessage);
+      });
+
+      it('confirms the plane is not flying', function() {
+        plane.land(airport);
+        expect(plane.isFlying).toEqual(false);
+      });
+    });
+
+    describe('#takeOff', function() {
+
+      beforeEach(function() {
+        plane.land(airport);
+      });
+
+      it('confirms the plane is flying', function() {
+        plane.takeOff(airport);
+        expect(plane.isFlying).toEqual(true);
+      });
+
+      it('confirms departure with a message', function() {
+        departureMessage = 'The plane has departed the airport';
+        expect(plane.takeOff(airport)).toEqual(departureMessage);
+      });
+    });
+  });
+
+
+  describe('Weather is stormy', function() {
+
+    // describe('#land', function() {
+    //
+    // });
+
+    describe('#takeOff', function() {
+      it('raises an error when the weather is stormy', function() {
+        error = "Cannot take off when weather is stormy";
+        plane1 = new Plane();
+        spyOn(plane1.weather, 'isStormy').and.returnValue(true);
+        expect(function () { plane1.takeOff(airport); }).toThrowError( error );
+      });
     });
   });
 });
